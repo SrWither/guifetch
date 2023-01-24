f5f69a6374353ee4c8f808ea69f53b14a1e7265b
Fadel <59105868+SrWither@users.noreply.github.com>
2023-01-24 00:20:22 -0300

    Add files via upload

--- src/getData.cpp
+++ src/getData.cpp
@@ -24,45 +24,21 @@ getData::~getData() {
 }
 
 int getData::getFreeRam() {
-  FILE *meminfo = fopen("/proc/meminfo", "r");
-  if(meminfo == NULL){
-    qDebug() << "Error al obtener informacion sobre la memoria ram";
-  }
+  struct sysinfo info;
+  sysinfo(&info);
 
-  char line[256];
-  while(fgets(line, sizeof(line), meminfo)){
-    int freeram;
+  m_osFreeMemory = info.freeram + info.bufferram;
 
-    if(sscanf(line, "MemAvailable: %d kB", &freeram) == 1) {
-      fclose(meminfo);
-      m_osFreeMemory = freeram / 1024;
-      return m_osFreeMemory;
-    }
-  }
-
-  fclose(meminfo);
-  return -1;
+  return m_osFreeMemory / 1024;
 }
 
 int getData::getTotalRam() {
-  FILE *meminfo = fopen("/proc/meminfo", "r");
-  if(meminfo == NULL){
-    qDebug() << "Error al obtener informacion sobre la memoria ram";
-  }
+  struct sysinfo info;
+  sysinfo(&info);
 
-  char line[256];
-  while(fgets(line, sizeof(line), meminfo)){
-    int totalram;
+  m_osTotalMemory = info.totalram;
 
-    if(sscanf(line, "MemTotal: %d kB", &totalram) == 1) {
-      fclose(meminfo);
-      m_osTotalMemory = totalram / 1024;
-      return m_osTotalMemory;
-    }
-  }
-
-  fclose(meminfo);
-  return -1;
+  return m_osTotalMemory / 1024;
 }
 
 int getData::getUptime() {
